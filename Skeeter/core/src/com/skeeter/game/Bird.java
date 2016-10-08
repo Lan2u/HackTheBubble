8dfe8d92f9faaf6558dc25565408f7e24a227dad
arianehine <arimau5@hotmail.co.uk>
2016-10-08 14:38:45 +0100

    subclasses

--- Skeeter/core/src/com/skeeter/game/Bird.java
+++ Skeeter/core/src/com/skeeter/game/Bird.java
@@ -4,6 +4,35 @@ package com.skeeter.game;
  * Created by aah4 on 08/10/16.
  */
 public class Bird {
-    
 
+    Bird(){
+
+    }
+    double position_x;
+    double position_y;
+    double direction;
+    int x_vel;
+    int y_vel;
+    int y_acceleration;
+    int hitpoints;
+
+
+    @Override
+    public void update{
+
+
+    }
+
+    public void draw{
+
+    }
+
+    public void movement{
+
+
+
+    }
+
+    }
 }
+
