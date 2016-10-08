ad86fbf87b13e51b83d4dc77e3df5cce04485dfe
Lan2u <pl59@st-andrews.ac.uk>
2016-10-08 11:28:18 +0100

    Added application dimensions (800x450), Added a super class (GameSprite) which all game objects which move extends, Began input processing

---
+++ Skeeter/core/src/com/skeeter/game/GameSprite.java
@@ -0,0 +1,20 @@
+package com.skeeter.game;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by Paul Lancaster on 08/10/16
+ */
+public class GameSprite extends Sprite {
+
+    @Override
+    public void draw(Batch batch){
+        System.out.println("ERROR SOMETHING DIDN'T OVERWRITE DRAW");
+    }
+
+    public void update(){
+
+    }
+}
