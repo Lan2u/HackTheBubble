e0999c99b5f1dbea5b1c20bd4798328b1714bc8d
Lan2u <pl59@st-andrews.ac.uk>
2016-10-08 11:27:06 +0100

    Added application dimensions (800x450), Added a super class (GameSprite) which all game objects which move extends, Began input processing

--- Skeeter/core/src/com/skeeter/game/Bird.java
+++ Skeeter/core/src/com/skeeter/game/Bird.java
@@ -1,9 +1,11 @@
 package com.skeeter.game;
 
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 /**
  * Created by aah4 on 08/10/16.
  */
-public class Bird {
+public class Bird extends GameSprite{
     
 
 }
--- Skeeter/core/src/com/skeeter/game/Skeeter.java
+++ Skeeter/core/src/com/skeeter/game/Skeeter.java
@@ -2,35 +2,105 @@ package com.skeeter.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Skeeter extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+import java.util.ArrayList;
+
+public class Skeeter extends ApplicationAdapter implements InputProcessor{
+
+    // Only public thing avaliable outside of this class
+    // List of all sprites currently in play
+	public static ArrayList<GameSprite> sprites = new ArrayList<GameSprite>();
+
+    // Private not accessable outside of class
+    private SpriteBatch batch;
+    private Texture backImg;
+
 	@Override
-	// called when the program starts (main)
+	// Called when the program starts (main)
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
-	//called every frame
+	// Called every frame
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Physics Update
+        update();
+
+        // Draw
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+        batch.draw(backImg,0,0); // Draw the background image
+
+		for (GameSprite sprite: sprites){
+		    sprite.draw(batch);
+        }
 		batch.end();
 	}
+
+	private void update(){
+	    for (GameSprite sprite: sprites){ // Called each frame to allow the sprite to process the physics
+	        sprite.update();
+        }
+    }
 	
 	@Override
-	//called at end of game
+	// Called at end of game
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		backImg.dispose();
 	}
+
+	/* INPUT PROCESSING */
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    // Not required but had to be added
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
