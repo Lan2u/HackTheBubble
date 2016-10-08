c567e34f0e54a1b96d4d90812ab4796821dd39ea
Paul Lancaster <pl59@st-andrews.ac.uk>
2016-10-08 10:15:11 +0100

    Initial Commit

---
+++ Skeeter/desktop/src/com/skeeter/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.skeeter.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.skeeter.game.Skeeter;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Skeeter(), config);
+	}
+}
