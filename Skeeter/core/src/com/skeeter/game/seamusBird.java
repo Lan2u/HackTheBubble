c7d0fed1b896a5a616cd20fc8dd7634e015e90c7
Seamus Bonner <smb28@st-andrews.ac.uk>
2016-10-08 12:30:29 +0100

    Bird Object Creation

--- Skeeter/core/src/com/skeeter/game/seamusBird.java
+++ Skeeter/core/src/com/skeeter/game/seamusBird.java
@@ -1,2 +1,14 @@
-public class seamusBird{
+package com.skeeter.game;
+
+public class Bird{
+
+    Bird(){
+        double positionX;
+        double positionY;
+        double yVelocity;
+        double yAcceleration;
+        double direction;
+        int hitPoints;
+    }
+
 }
